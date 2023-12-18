e52df539898f503e79e29edb2b2b50323bf9bb37
Norayr <asoyannorayr99@gmail.com>
2023-12-18 18:40:40 +0300

    add commit

--- src/Student.java
+++ src/Student.java
@@ -1,32 +1,25 @@
 public class Student {
     public static void main(String[] args) {
-        JavaStudent javaStudent = new JavaStudent();
-        System.out.println(javaStudent.getInfo());
+        Student student = new Student();
+        student.study();
+    }
+    protected final String studying;
+
+    protected Student(String work) {
+        this.studying = work;
     }
 
-    public static class JavaStudent extends Student {
-        public JavaStudent() {
-            super("Я очень занят. Прохожу курс по Java.");
-        }
-
-        public String getInfo() {
-            return "Студент: " + this.getName() + ". " + this.getDescription();
-        }
+    public Student() {
+        this.studying = "Прохожу тестовое задание.";
     }
 
-    private String name;
-    private String description;
-
-    public Student(String description) {
-        this.name = "JavaStudent";
-        this.description = description;
+    public void study() {
+        System.out.println("Я очень занят. " + studying);
     }
+}
 
-    public String getName() {
-        return this.name;
-    }
-
-    public String getDescription() {
-        return this.description;
+class JavaStudent extends Student {
+    public JavaStudent() {
+        super("Я очень занят. Прохожу курс по Java.");
     }
 }
